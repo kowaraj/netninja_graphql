25a8c26f122a24cca6c9d8ef546f02fc79015846
kowaraj <kowaraj4stuff@gmail.com>
2019-12-29 17:12:02 +0100

    First release

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+const mlab_login = require('./login_to_mlab')
+
+const app = express()
+
+mlab_login(mongoose);
+mongoose.connection.once('open', ()=>{console.log("Connected to MongoDB instance!")})
+
+// Express doesn't know how to handle GraphQL, 
+// so it will pass the requests to...
+app.use('/graphql', graphqlHTTP({
+    schema: schema, 
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log("Listening on port 4000...")
+})
---
+++ schema/schema.js
@@ -0,0 +1,133 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const Book = require('../models/book');
+const Author = require('../models/author')
+
+const { 
+    GraphQLObjectType, 
+    GraphQLString, 
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt, 
+    GraphQLList
+} = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: {type: GraphQLID}, 
+        name: {type: GraphQLString},
+        title: {type: GraphQLString}, 
+        author: {
+            type: AuthorType, 
+            resolve(parent, args){
+                //return _.find(authors, {id: parent.authorId} )
+            }
+        }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLID}, 
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt},
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                console.log(parent)
+                console.log(args)
+                //return _.filter(books, {authorId: args.id })
+            }
+        }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType', 
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLID}}, 
+            resolve(parent, args){
+                // code to get data...
+                // parents - from the relationships
+                // args - to get a book with id = args.id
+                // { data: "test data"}
+                console.log(typeof(args.id))
+                //return _.find(books, {id: args.id} );
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: {id: {type: GraphQLID}}, 
+            resolve(parent, args){
+                //return _.find(authors, {id: args.id} );
+            }
+        }, 
+        books: {
+            type: new GraphQLList(BookType), 
+            resolve(parent, args) {
+                //return books 
+            }
+        }, 
+        authors: {
+            type: new GraphQLList(AuthorType), 
+            resolve(parent, args) {
+                //return authors
+            }
+        }
+
+    }
+});
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation', 
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: {type: GraphQLString},
+                age: {type: GraphQLInt}
+            },
+            resolve(parent, args){
+                let a = new Author({
+                    name: args.name, 
+                    age: args.age
+                });
+                return a.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLID}
+            },
+            resolve(parent, args){
+                let book = new Book({
+                    name: args.name, 
+                    genre: args.genre,
+                    authorId: args.authorId
+                });
+                return book.save();
+            }
+        }
+
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery, 
+    mutation: Mutation 
+});
+
+
+// mutation{
+// 	addAuthor(name:"Andrey", age:66) {
+//     name
+// 		age
+// 	}
+// }
