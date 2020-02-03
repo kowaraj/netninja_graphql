880239b3cf156c2f6cc6f0ef7afd66e91f8031fb
kowaraj <kowaraj4stuff@gmail.com>
2020-02-03 18:34:38 +0100

    Add a display of the book's details

--- frontend/src/components/BookDetails.js
+++ frontend/src/components/BookDetails.js
@@ -2,13 +2,33 @@ import React from 'react';
 import {getBookQuery} from '../queries/queries';
 import {graphql } from 'react-apollo';
 
+function displayDetails(props) {
+    const {book} = props.data;
+    if(book) {
+        return (
+            <div>
+                <h2> {book.name}</h2>
+                <p> {book.author.name}</p>
+                <p> All books: </p>
+                <ul>
+                    { book.author.books.map( b=> { return <li key={b.id}>{b.name}</li>})}
+                </ul>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div> No book selected... </div>
+        )
+    }
+}
 
 function BookDetails(props) {
     console.log(props);
     return (
         <div id="book-details"> 
             <p> Output book details here... </p>
-
+            {displayDetails(props)}
         </div>
     );
 }
